cb7e5e1a69abd5a1a96228aa2506fa09ef74740d
Exequiel Barros <exebarros_9@hotmail.com.ar>
2025-07-27 20:29:16 -0300

    tp terminado

---
+++ Tareas/Tareas.cs
@@ -0,0 +1,17 @@
+using System.Dynamic;
+using System.Text.Json.Serialization;
+
+namespace TareaSpace
+{
+    public class Tarea
+    {
+        [JsonPropertyName("userId")]
+        public int UserId { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+        [JsonPropertyName("completed")]
+        public bool Completed { get; set; }
+    }
+}
